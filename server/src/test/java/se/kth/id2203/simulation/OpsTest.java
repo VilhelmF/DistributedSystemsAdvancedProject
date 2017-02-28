2b087e96ad7846c6e16fba0760429842b00887ad
Sindri Már Kaldal Sigurjónsson <sindrikaldal@n170-p34.kthopen.kth.se>
2017-02-28 20:59:10 +0100

    further work on multi paxos. Is leader necessary?

--- server/src/test/java/se/kth/id2203/simulation/OpsTest.java
+++ server/src/test/java/se/kth/id2203/simulation/OpsTest.java
@@ -46,7 +46,7 @@ public class OpsTest {
     public void simpleGetTest() {
         long seed = 123;
         SimulationScenario.setSeed(seed);
-        SimulationScenario simpleBootScenario = ScenarioGen.simpleOps(6);
+        SimulationScenario simpleBootScenario = ScenarioGen.simpleOps(3);
         res.put("messages", NUM_MESSAGES);
         simpleBootScenario.simulate(LauncherComp.class);
         for (int i = 0; i < NUM_MESSAGES; i++) {
@@ -59,7 +59,7 @@ public class OpsTest {
     public void simplePutTest() {
         long seed = 123;
         SimulationScenario.setSeed(seed);
-        SimulationScenario simpleBootScenario = ScenarioGen.simpleOps(6);
+        SimulationScenario simpleBootScenario = ScenarioGen.simpleOps(3);
         res.put("messages", NUM_MESSAGES);
         simpleBootScenario.simulate(LauncherComp.class);
         for (int i = 0; i < NUM_MESSAGES; i++) {
